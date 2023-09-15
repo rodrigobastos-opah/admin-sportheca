10bc5b0fa1daf4a763f01e72239f26902be8ded4
rodrigobastos-opah <rodrigo.bastos@opah.com.br>
2023-09-15 00:29:00 -0300

    feat: ajustes tela login

--- sportheca-admin/src/pages/Login/index.tsx
+++ sportheca-admin/src/pages/Login/index.tsx
@@ -1,8 +1,17 @@
+import * as React from 'react';
 import * as St from './styles';
-import Logo from '../../assets/images/logo-sportheca.png'
+import Logo from '../../assets/images/logo-sportheca.png';
+import Formas from '../../assets/images/formas.png'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import InputLabel from '@mui/material/InputLabel';
+import IconButton from '@mui/material/IconButton';
 import { styled } from '@mui/material/styles';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const CustomTextField = styled(TextField)({
   '& label': {
@@ -14,8 +23,11 @@ const CustomTextField = styled(TextField)({
   '& .MuiInput-underline:after': {
     borderBottomColor: '#B2BAC2',
   },
+  '.MuiInputBase-input': {
+    color: '#A0AAB4',
+  },
   '& .MuiOutlinedInput-root': {
-    width: 500,
+    width: 450,
     marginBottom: 24,
     '& fieldset': {
       borderColor: '#E0E3E7',
@@ -29,37 +41,124 @@ const CustomTextField = styled(TextField)({
   },
 });
 
-const CustomButton = styled(Button) ({
+const CustomPassword = styled(OutlinedInput)({
+  '& .MuiInputLabel-root': {
+    color: '#A0AAB4!important',
+  },
+  '& label.Mui-focused': {
+    color: '#A0AAB4',
+  },
+  '& fieldset': {
+    borderColor: '#E0E3E7',
+  },
+  '&:hover fieldset': {
+    borderColor: '#B2BAC2!important',
+  },
+  '&.Mui-focused fieldset': {
+    borderColor: '#6F7E8C !important',
+  },
+});
+
+const CustomButton = styled(Button)({
   backgroundColor: 'transparent',
   border: '2px solid #ff004f',
   fontFamily: 'Lato',
   fontWeight: 'bold',
-  borderRadius: 0,
   width: '100%',
-  height: 42,
+  height: 60,
+  fontSize: '1.2rem',
+  textTransform: 'inherit',
+  marginTop: 24,
   '&:hover': {
-    backgroundColor: 'green',
+    backgroundColor: '#ff004f',
   }
 });
 
 export const Login = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <St.Container>
+      <St.Forms src={Formas} alt="Formas" />
       <St.Content>
         <St.Logo src={Logo} alt="Sportheca" />
-        <St.PageTitle>Entrar</St.PageTitle>
-        <CustomTextField
-          required
-          id="outlined-required"
-          label="Login"
-        />
-        <CustomTextField
-          id="outlined-password-input"
-          label="Senha"
-          type="password"
-          autoComplete="current-password"
-        />
-        <CustomButton variant="contained">Entrar</CustomButton>
+        <St.SectionForm>
+          <CustomTextField
+            required
+            id="outlined-required"
+            label="Login"
+          />
+          <FormControl variant="outlined">
+            <InputLabel
+              htmlFor="outlined-adornment-password"
+              style={{ color: '#A0AAB4' }}
+            >Senha *
+            </InputLabel>
+            <CustomPassword
+              id="outlined-adornment-password"
+              type={showPassword ? 'text' : 'password'}
+              style={{ color: '#A0AAB4'}}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="Mostar senha"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                    style={{ color: '#A0AAB4'}}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          {/* <CustomTextField
+            id="outlined-password-input"
+            label="Senha"
+            type="password"
+            autoComplete="current-password"
+            required
+          />
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          /> */}
+          <St.ForgotPass>Esqueceu sua senha?</St.ForgotPass>
+          <CustomButton variant="contained">Entrar</CustomButton>
+        </St.SectionForm>
+        <St.HelperSection>
+          <St.HelperColumn>
+            <p>Novo por aqui?</p>
+            <a href="">Conheça nossa plataforma</a>
+          </St.HelperColumn>
+          <St.Divisor />
+          <St.HelperColumn>
+            <p>Algum problema?</p>
+            <a href="">Fale Conosco</a>
+          </St.HelperColumn>
+        </St.HelperSection>
+        <St.Register>
+          Cadastre-se
+        </St.Register>
       </St.Content>
     </St.Container>
   );
--- sportheca-admin/src/pages/Login/styles.ts
+++ sportheca-admin/src/pages/Login/styles.ts
@@ -6,8 +6,18 @@ export const Container = styled.main`
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding-top: 10%;
+    justify-content: center;
     background: linear-gradient(160deg, #42006C 0%, #000000 100%);
+    overflow: hidden;
+`;
+
+export const Forms = styled.img`
+  position: absolute;
+  z-index: 0;
+  left: -10px;
+  top: 0;
+  width: 45%;
+  opacity: 0.2; 
 `;
 
 export const Content = styled.section`
@@ -15,24 +25,72 @@ export const Content = styled.section`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  background: #160520;
+  width: 90%;
+  border-radius: 20px;
+  padding: 50px 25px;
+  height: 75vh;
+  z-index: 9;
 `;
 
 export const Logo = styled.img`
   width: 320px;
 `;
 
-export const PageTitle = styled.h1`
-  color: #FFFFFF;
-  font-family: 'Lato';
-  font-size: 2.5rem;
-  padding-top: 24px;
+export const SectionForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 5%;
 `;
 
-export const Button = styled.button`
-
-    &:hover {
-
-    }
+export const ForgotPass = styled.p`
+  margin: 7px 0 30px 0;
+  padding: 0;
+  text-align: right;
+  width: 100%;
+  font-size: 13px;
+  color: #B2BAC2;
 `;
 
-export const TextField = styled.input``;
+export const HelperSection = styled.div`
+    background-color: rgb(58, 58, 58, 0.5);
+    display: flex;
+    width: 450px;
+    margin-top: 24px;
+    border-radius: 6px;
+    padding: 10px 15px 20px;
+    box-sizing: border-box;
+    line-height: 8px;
+    justify-content: space-around;
+`;
+
+export const HelperColumn = styled.div`
+    width: 50%;
+    text-align: center;
+  p {
+    color: #B2BAC2;
+    font-size: 12px;
+    font-family: 'Lato';
+  }
+
+  a {
+    color: #ff004f;
+    font-size: 14px;
+    font-family: 'Lato';  
+  }
+`;
+
+export const Divisor = styled.div`
+    width: 2px;
+    background-color: rgba(255, 0, 79, 0.5);
+    height: 30px;
+    margin-top: 10px;
+`;
+
+export const Register = styled.button`
+    width: 450px;
+    margin-top: 24px;
+    font-family: 'Lato';  
+    font-size: 16px;
+    height: 60px;
+`;
